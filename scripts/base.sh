fa4a69a9441f5397d96d9f0f0b3511b4e7048a36
Guy Silva <gsivla.85@gmail.com>
2014-09-16 13:02:15 +0100

    latest modifications

--- scripts/base.sh
+++ scripts/base.sh
@@ -6,6 +6,8 @@ apt-get -y upgrade
 apt-get -y install gcc build-essential linux-headers-$(uname -r)
 apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
 apt-get -y install vim curl
+# Install NFS client
+apt-get -y install nfs-common
 apt-get clean
 
 # Set up sudo
@@ -33,5 +35,4 @@ EOF
 
 update-grub
 
-# Install NFS client
-apt-get -y install nfs-common
+
